22a9d45a95335a7702e0940cb7c1c7128bdd89af
Erick Hernandez <desarrollo.softsware3@prosur.com.mx>
2023-07-26 12:24:33 -0500

    social media

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 // Cuando el documento está listo
 document.addEventListener('DOMContentLoaded', function () {
+    onScreenAnimation()
     window.addEventListener('scroll', (ev1) => {
-        // y a todos los elementos con la clase paused...
-        document.querySelectorAll(".paused").forEach(elm => {
-            if (isElementVisible(elm)){// que sean visibles...
-                elm.classList.remove("paused"); // les quitamos la clase paused
-                elm.classList.add("on_screen");
-            } 
-        })
-
-        document.querySelectorAll(".on_screen").forEach(elm => {
-            if (!isElementVisible(elm)){// que sean visibles...
-                elm.classList.remove("on_screen"); // les quitamos la clase paused
-                elm.classList.add("paused");
-            } 
-        })
         
+        onScreenAnimation()
     });
     
     scrollanimation()
 });
 
+function onScreenAnimation() {
+    // y a todos los elementos con la clase paused...
+    document.querySelectorAll(".paused").forEach(elm => {
+        if (isElementVisible(elm)){// que sean visibles...
+            elm.classList.remove("paused"); // les quitamos la clase paused
+            elm.classList.add("on_screen");
+        } 
+    })
+
+    document.querySelectorAll(".on_screen").forEach(elm => {
+        if (!isElementVisible(elm)){// que sean visibles...
+            elm.classList.remove("on_screen"); // les quitamos la clase paused
+            elm.classList.add("paused");
+            
+            elm.querySelectorAll(".anim").forEach(elm2 => {
+                //obtenemos todas las clases que contengan anim , se las quitamos y se las volvemos a poner para que se reinicie la animacion
+                var classes = elm2.classList;
+                var classesArray = Array.from(classes);
+                classesArray.forEach(element => {
+                    if(element.includes("anim")){
+                        elm2.classList.remove(element);
+                        elm2.classList.add(element);
+                    }
+                }
+                );
+            } )
+        } 
+    })
+}
 
 
 
@@ -38,13 +55,28 @@ function scrollanimation() {
             // Obtenemos el atributo href del enlace para encontrar la sección objetivo
             const targetId = this.getAttribute('href');
 
-            // Obtenemos la posición vertical de la sección objetivo
+            if (targetId.includes("#")) {
+                // Obtenemos la posición vertical de la sección objetivo
             const targetPosition = document.querySelector(targetId).offsetTop;
             // Realizamos el scroll suave a la posición de la sección objetivo
             window.scroll({
                 top: targetPosition,
                 behavior: 'smooth'
             });
+                return
+            }
+
+            const target = this.getAttribute('target');
+            if (target) {
+                window.open(targetId, 'target');
+                return
+            }
+            window.open(targetId, '_self');
+
+            //navegar al link si no es un link interno
+            
+            
+            
 
         });
     });
@@ -53,13 +85,8 @@ function scrollanimation() {
 
 
 //funcion para ver si un elmento es visible en la pantalla
-function isElementVisible(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        (rect.top <= 0 && rect.bottom >= 0)
-        ||
-        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-        ||
-        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    );
+function isElementVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+	var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
